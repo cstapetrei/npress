489db0e9584de12949311eac8cccd3ee3661ff93
Constantin Apetrei <cstapetrei@yahoo.com>
2020-07-05 00:49:11 +0300

    Continue work on page builder

--- src/static/vendor/npress-page-builder.js
+++ src/static/vendor/npress-page-builder.js
@@ -12,26 +12,74 @@ NPress.PageBuilder = class PageBuilder{
 
         this.addToolbar();
         NPress.live(this.options.el, 'click', '.js-add-col', this.onAddColumn);
-        NPress.live(this.options.el, 'click', '.js-add-element', (e, el) => {
-            e.preventDefault();
-            let r = el.closest('.npress-page-builder-widget-toolbar').parentNode.querySelector('.col-content');
-            let item = document.createElement('h1');
-            item.innerHTML = 'Test';
-            r.appendChild(item);
+        NPress.live(this.options.el, 'click', '.js-remove-element', this.onRemoveElement);
+        NPress.live(this.options.el, 'click', '.js-add-element', this.onAddElement);
+    }
+    onAddElement(e, el){
+        e.preventDefault();
+        let content = el.closest('[class^="col-"]').querySelector('.col-content');
+        let modal = new NPress.Modal({
+            content: `
+                <label>Text<input type="radio" name="el" value="text"></label>
+                <label>Image<input type="radio" name="el" value="img"></label>
+                <label>Code<input type="radio" name="el" value="code"></label>
+            `,
+            title: 'Select element',
+            buttons: [
+                {
+                    label: 'Cancel',
+                    className: 'btn btn-info'
+                },
+                {
+                    label: 'Select',
+                    className: 'btn btn-primary',
+                    onClick: (e, modal) => {
+                        let inp = modal.modalBody.querySelector('input:checked');
+                        if (inp){
+                            let type = inp.value;
+                            let div = document.createElement('div');
+                            div.className = 'npress-page-builder-widget';
+                            switch(type){
+                                case 'text':
+                                    div.innerHTML = 'Placeholder text';
+                                    break;
+                                case 'img':
+                                    div.innerHTML = '<img src="" class="img-fluid" />';
+                                    break;
+                                case 'code':
+                                    div.innerHTML = '<code>Placeholder text</code>';
+                                    break;
+                            }
+                            content.appendChild(div);
+                        }
+                    }
+                }
+            ]
         });
+    }
+    onRemoveElement(e, el){
+        let isColumn = el.getAttribute('data-is-column');
+        let widget = el.closest('.npress-page-builder-widget');
+        if (isColumn){
+            widget = el.closest('[class^="col-"]');
+        }
+        if (widget){
+            widget.remove();
+        }
     }
     addToolbar(){
         this.toolbarNode = document.createElement('aside');
         this.toolbarNode.className = 'npress-page-builder-widget-toolbar';
         this.toolbarNode.innerHTML = `
-            <button class="btn btn-default js-add-container" data-class="container">boxed</button>
-            <button class="btn btn-default js-add-fluid-container" data-class="container-fluid">fluid</button>
-            <button class="btn btn-default js-get-code">code</button>
+            <button class="btn btn-default js-add-container btn-xs" data-class="container" title="Boxed" data-toggle="tooltip"><i class="fas fa-angle-right"></i> <i class="fas fa-angle-left"></i></button>
+            <button class="btn btn-default js-add-fluid-container btn-xs" data-class="container-fluid" title="Fluid" data-toggle="tooltip"><i class="fas fa-angle-double-left"></i> <i class="fas fa-angle-double-right"></i></button>
+            <button class="btn btn-default js-get-code btn-xs" title="Switch to source" data-toggle="tooltip"><i class="fas fa-code"></i></button>
         `;
         this.options.el.appendChild(this.toolbarNode);
 
         NPress.live(this.toolbarNode, 'click', '.js-add-container', this.onAddContainer.bind(this));
         NPress.live(this.toolbarNode, 'click', '.js-add-fluid-container', this.onAddContainer.bind(this));
+        NPress.live(this.toolbarNode, 'click', '.js-get-code', (e, el) => { e.preventDefault() });
     }
     onAddColumn(e, el){
         e.preventDefault();
@@ -42,7 +90,8 @@ NPress.PageBuilder = class PageBuilder{
         c.innerHTML = `
             <div class="npress-page-builder-widget col-content"></div>
             <div class="npress-page-builder-widget-toolbar">
-                <button class="btn btn-default js-add-element">add</button>
+                <button class="btn btn-default btn-xs js-add-element"><i class="fas fa-plus"></i></button>
+                <button class="btn btn-danger btn-xs js-remove-element" data-is-column="1"><i class="fas fa-times"></i></button>
             </div>
         `;
         r.appendChild(c);
@@ -54,8 +103,9 @@ NPress.PageBuilder = class PageBuilder{
         c.className = `${containerClass} npress-page-builder-widget`;
         let btnHtml = '';
         for (let i = 1; i <= Array(12).length; i++){
-            btnHtml += `<button class="btn btn-default btn-xs js-add-col" data-class="${i}">${i}</button>`;
+            btnHtml += `<button class="btn btn-default btn-xs js-add-col mx-1" data-class="${i}">${i}/12</button>`;
         }
+        btnHtml += `<button class="btn btn-danger btn-xs js-remove-element"><i class="fas fa-times"></i></button>`;
         c.innerHTML = `<div class="row"></div><div class="npress-page-builder-widget-toolbar">${btnHtml}</div>`;
         this.options.el.insertBefore(c, this.toolbarNode);
     }
