a62b5bf478c26aa12d0f69af00bc8b93c06166e9
Constantin Apetrei <constantin.apetrei@emag.ro>
2020-07-07 14:12:23 +0300

    work on page builder

--- src/static/vendor/npress-page-builder.js
+++ src/static/vendor/npress-page-builder.js
@@ -11,56 +11,66 @@ NPress.PageBuilder = class PageBuilder{
         }
 
         this.addToolbar();
-        NPress.live(this.options.el, 'click', '.js-add-col', this.onAddColumn);
+        NPress.live(this.options.el, 'click', '.js-add-col', this.onAddColumn.bind(this));
         NPress.live(this.options.el, 'click', '.js-remove-element', this.onRemoveElement);
-        NPress.live(this.options.el, 'click', '.js-add-element', this.onAddElement);
     }
     onAddElement(e, el){
         e.preventDefault();
         let content = el.closest('[class^="col-"]').querySelector('.col-content');
-        let modal = new NPress.Modal({
-            content: `
-                <label>Text<input type="radio" name="el" value="text"></label>
-                <label>Image<input type="radio" name="el" value="img"></label>
-                <label>Code<input type="radio" name="el" value="code"></label>
-            `,
-            title: 'Select element',
-            buttons: [
-                {
-                    label: 'Cancel',
-                    className: 'btn btn-info'
-                },
-                {
-                    label: 'Select',
-                    className: 'btn btn-primary',
-                    onClick: (e, modal) => {
-                        let inp = modal.modalBody.querySelector('input:checked');
-                        if (inp){
-                            let type = inp.value;
-                            let div = document.createElement('div');
-                            div.className = 'npress-page-builder-widget';
-                            switch(type){
-                                case 'text':
-                                    div.innerHTML = 'Placeholder text';
-                                    break;
-                                case 'img':
-                                    div.innerHTML = '<img src="" class="img-fluid" />';
-                                    break;
-                                case 'code':
-                                    div.innerHTML = '<code>Placeholder text</code>';
-                                    break;
-                            }
-                            content.appendChild(div);
-                        }
-                    }
-                }
-            ]
-        });
+        let type = el.getAttribute('data-type');
+        if (!type){
+            return;
+        }
+        let div = document.createElement('div');
+        div.className = 'npress-page-builder-widget text-center';
+        switch(type){
+            case 'text':
+                div.innerHTML += 'Placeholder text';
+                break;
+            case 'img':
+                div.innerHTML += '<img src="" class="img-fluid" />';
+                break;
+            case 'code':
+                div.innerHTML += '<code>Placeholder text</code>';
+                break;
+        }
+        div.innerHTML = `
+            <div class="npress-page-builder-widget-content">${div.innerHTML}</div>
+            <div class="npress-page-builder-widget-toolbar">
+                <button class="btn btn-default btn-xs js-edit-element" data-type="${type}"><i class="fas fa-edit"></i></button>
+                <button class="btn btn-danger btn-xs js-remove-element" data-is-element="1"><i class="fas fa-times"></i></button>
+            </div>
+        `;
+        NPress.live(div, 'click', '.js-edit-element', this.onEditElement.bind(this));
+        content.appendChild(div);
     }
+
+    onEditElement(e, el){
+        let content = el.closest('.npress-page-builder-widget').querySelector('.npress-page-builder-widget-content');
+        let type = el.getAttribute('data-type');
+        if (!type){
+            return;
+        }
+        switch(type){
+            case 'text':
+
+                break;
+            case 'img':
+
+                break;
+            case 'code':
+
+                break;
+        }
+    }
+
     onRemoveElement(e, el){
         let isColumn = el.getAttribute('data-is-column');
+        let isElement = el.getAttribute('data-is-element');
         let widget = el.closest('.npress-page-builder-widget');
-        if (isColumn){
+        if (isElement){
+            widget = el.parentNode.parentNode;
+        } else if (isColumn){
             widget = el.closest('[class^="col-"]');
         }
         if (widget){
@@ -90,10 +100,20 @@ NPress.PageBuilder = class PageBuilder{
         c.innerHTML = `
             <div class="npress-page-builder-widget col-content"></div>
             <div class="npress-page-builder-widget-toolbar">
-                <button class="btn btn-default btn-xs js-add-element"><i class="fas fa-plus"></i></button>
+                <div class="dropdown">
+                    <button class="btn btn-default btn-xs" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i class="fas fa-plus"></i>
+                    </button>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#" data-type="text">Text</a>
+                        <a class="dropdown-item" href="#" data-type="image">Image</a>
+                        <a class="dropdown-item" href="#" data-type="code">Code</a>
+                    </div>
+                </div>
                 <button class="btn btn-danger btn-xs js-remove-element" data-is-column="1"><i class="fas fa-times"></i></button>
             </div>
         `;
+        NPress.live(c, 'click', '.dropdown-menu a', this.onAddElement.bind(this));
         r.appendChild(c);
     }
     onAddContainer(e, el){
