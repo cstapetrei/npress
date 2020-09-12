388d5f9c4b2b53788da7e537b7d635eaa12403b9
Constantin Apetrei <cstapetrei@yahoo.com>
2020-09-12 12:32:23 +0300

    Added momentjs with locales Updated packages Remove comment controller param

--- src/controllers/PublicCommentApiController.ts
+++ src/controllers/PublicCommentApiController.ts
@@ -10,7 +10,7 @@ import { BadRequestException } from "../library/exceptions/BadRequestException";
 export class PublicCommentApiController{
     public static async getAll(req: Request, res: Response) {
         try{
-            let result: [Comment[], number] = await (Container.get("CommentService") as CommentService).getPaged(parseInt(req.query.page as string)-1, 10, '', true)
+            let result: [Comment[], number] = await (Container.get("CommentService") as CommentService).getPaged(parseInt(req.query.page as string)-1, 10, '')
             return CountHeadersResponse(result[0], result[1], res);
         } catch(e){
             APIExceptionResponse(e,res);
